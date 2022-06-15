27da8fd5b4cddca4b55b4a3944724144b5fa4232
David Sikes <pockybum522@gmail.com>
2022-06-15 17:19:19 -0400

    Added DnD monster models and logic

---
+++ OpenAiDndCampaignStarter/Data/Logic/MonsterTableHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using OpenAiDndCampaignStarter.Data.Models;
+using OpenAiDndCampaignStarter.Data.MonsterTables;
+
+namespace OpenAiDndCampaignStarter.Data.Logic;
+
+public class MonsterTableHelpers
+{
+    public static DndMonster GetRandom5eMonster()
+    {
+        var table5eLength = Monsters5e.Monsters.Length;
+        
+        var randomNumber = RandomNumberGenerator.GetInt32(0, table5eLength - 1);
+        
+        return Monsters5e.Monsters[randomNumber];
+    }
+}
---
+++ OpenAiDndCampaignStarter/Data/Models/DndMonster.cs
@@ -0,0 +1,48 @@
+﻿using JetBrains.Annotations;
+
+namespace OpenAiDndCampaignStarter.Data.Models;
+
+[PublicAPI]
+public class DndMonster
+{
+    public DndMonster(
+        string creatureName, 
+        string size, 
+        string type, 
+        string subType, 
+        string lawful, 
+        string alignment, 
+        int cr, 
+        int experience, 
+        string biome, 
+        string sourceMaterial, 
+        int sourcePageNumber,
+        string edition)
+    {
+        CreatureName = creatureName;
+        Size = size;
+        Type = type;
+        SubType = subType;
+        Lawful = lawful;
+        Alignment = alignment;
+        Cr = cr;
+        Experience = experience;
+        Biome = biome;
+        SourceMaterial = sourceMaterial;
+        SourcePageNumber = sourcePageNumber;
+        Edition = edition;
+    }
+    
+    public string CreatureName { get; }
+    public string Size { get; }
+    public string Type { get; }
+    private string SubType { get; }
+    public string Lawful { get; }
+    public string Alignment { get; }
+    public int Cr { get; }
+    public int Experience { get; }
+    public string Biome { get; }
+    public string SourceMaterial { get; }
+    public int SourcePageNumber { get; }
+    private string Edition { get; }
+}
