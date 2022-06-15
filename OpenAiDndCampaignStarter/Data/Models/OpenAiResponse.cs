bfd89d114e44d8adaded8a0f64115681ca111ed1
David Sikes <pockybum522@gmail.com>
2022-06-15 19:53:14 -0400

    Figured out the weirdest solution to pluralizing monster names

--- OpenAiDndCampaignStarter/Data/Models/OpenAiResponse.cs
+++ OpenAiDndCampaignStarter/Data/Models/OpenAiResponse.cs
@@ -2,17 +2,17 @@
 
 public class OpenAiResponse
 {
-    public string id { get; set; }
-    public string @object { get; set; }
+    public string id { get; set; } = "";
+    public string @object { get; set; } = "";
     public int created { get; set; }
-    public string model { get; set; }
-    public List<Choice> choices { get; set; }
+    public string model { get; set; } = "";
+    public List<Choice> choices { get; set; } = new();
 }
 
 public class Choice
 {
-    public string text { get; set; }
+    public string text { get; set; } = "";
     public int index { get; set; }
-    public object logprobs { get; set; }
-    public string finish_reason { get; set; }
+    public object? logprobs { get; set; }
+    public string finish_reason { get; set; } = "";
 }
