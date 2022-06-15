4fec8d2ba519f7e840f282fe5ed59a6806f7cd59
David Sikes <pockybum522@gmail.com>
2022-06-15 15:28:36 -0400

    Added OpenAiJsonRequest, working

---
+++ OpenAiDndCampaignStarter/Data/Models/OpenAiJsonRequest.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable InconsistentNaming because this is for serialization
+
+using JetBrains.Annotations;
+
+namespace OpenAiDndCampaignStarter.Data.Models;
+
+[PublicAPI]
+public class OpenAiJsonRequest
+{
+    public OpenAiJsonRequest(string promptString)
+    {
+        prompt = promptString;
+    }
+    
+    public string prompt { get; }
+    public string model { get; set; } = "text-davinci-002";
+    public double temperature { get; set; } = 0.8;
+    public int max_tokens { get; set; } = 240;
+    public double top_p { get; set; } = 1.0;
+    public double frequency_penalty { get; set; } = 0.5;
+    public double presence_penalty { get; set; }
+}
